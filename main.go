8bf7c8a0e3b9fe83cf129dcc16f360e431106c8c
victor <c9victor@yahoo.com>
2026-08-23 20:51:39 -0400

    file restructure. Caching added.

--- main.go
+++ main.go
@@ -1,35 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
-type cliCommand struct {
-	name        string
-	description string
-	callback    func() error
-}
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		text := scanner.Text()
-		words := cleanInput(text)
-		fmt.Print(words)
-		switch words[0] {
-		case "exit":
-			os.Exit(0)
-		default:
-			continue
-		}
-		// func commandExist() error {
-		// 	fmt.Print("Closing the Pokedex... Goodbye!")
-		// 	os.Exit(0)
-		// }
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,10 +1,73 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func(*config) error
+}
+
+type config struct {
+	commands map[string]cliCommand
+	prevUrl  string
+	nextUrl  string
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	cfg := &config{
+		commands: getCommands(),
+	}
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := scanner.Text()
+		words := cleanInput(text)
+
+		commandName := words[0]
+		cmd, exists := cfg.commands[commandName]
+		if exists {
+			err := cmd.callback(cfg)
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
+
+	}
+}
 
 func cleanInput(text string) []string {
 	trimmedText := strings.Trim(text, " ")
 	loweredText := strings.ToLower(trimmedText)
 	words := strings.Split(loweredText, " ")
 	return words
+}
+
+func getCommands() map[string]cliCommand {
+	commands := make(map[string]cliCommand)
+	commands["exit"] = cliCommand{
+		name:        "exit",
+		description: "Exits the Pokedex",
+		callback:    commandExit,
+	}
+	commands["help"] = cliCommand{
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	}
+	commands["map"] = cliCommand{
+		name:        "map",
+		description: "Displays the next 20 locations in the Pokemon world",
+		callback:    commandMap,
+	}
+	return commands
 }
